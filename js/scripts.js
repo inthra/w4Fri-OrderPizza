1905d57ec795c6319a309a7a54499edfa6942404
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-04-08 11:17:24 -0700

    modified html, js, css to be functioning

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@ function Pizza(top1, top2, top3, size, name) {
   this.name = name;
 }
 
+Pizza.prototype.totalPrice = function() {
+  var total = this.top1 + this.top2 + this.top3 + this.size;
+  return total;
+}
 
 
 // user interface logic ---------------
@@ -14,8 +18,15 @@ $(document).ready(function(){
   $("#order form").submit(function(event){
     event.preventDefault();
     var inputTop1 = parseInt($("input:radio[name=first]:checked").val());
-    alert(inputTop1)
+    var inputTop2 = parseInt($("input:radio[name=second]:checked").val());
+    var inputTop3 = parseInt($("input:radio[name=third]:checked").val());
+    var inputSize = parseInt($("input:radio[name=size]:checked").val());
+    var inputName = $("#name").val();
 
+    var newOrder = new Pizza(inputTop1, inputTop2, inputTop3, inputSize, inputName);
+    console.log(newOrder);
 
+    $("#total").text(newOrder.totalPrice());
+    $("#output").show();
   });
 });
