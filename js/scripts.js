d51744f4eb0a925d1cc7cd936ee7d3ee784e07d8
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-04-08 15:37:24 -0700

    change value of size in text and create prototype for calculating in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,29 @@ function Pizza(top1, top2, top3, size, name) {
   this.top3 = top3;
   this.size = size;
   this.name = name;
+};
+
+Pizza.prototype.sizePrice = function(size) {
+  var price = 7;
+
+  if (this.size === "small") {
+    return price;
+  }
+  else if (this.size === "medium") {
+    return price += 2;
+  }
+  else {
+    return price += 4;
+  }
 }
+
   // prototype for calculating total price -----
 Pizza.prototype.totalPrice = function() {
-  var total = this.top1 + this.top2 + this.top3 + this.size;
+  var total = this.top1 + this.top2 + this.top3 + this.sizePrice(this.size);
+
   return total;
-}
+};
+
 
 // user interface logic ---------------
 $(document).ready(function(){
@@ -20,7 +37,7 @@ $(document).ready(function(){
     var inputTop1 = parseInt($("input:radio[name=first]:checked").val());
     var inputTop2 = parseInt($("input:radio[name=second]:checked").val());
     var inputTop3 = parseInt($("input:radio[name=third]:checked").val());
-    var inputSize = parseInt($("input:radio[name=size]:checked").val());
+    var inputSize = $("input:radio[name=size]:checked").val();
     var inputName = $("#name").val();
 
     var newOrder = new Pizza(inputTop1, inputTop2, inputTop3, inputSize, inputName);
@@ -31,8 +48,9 @@ $(document).ready(function(){
     $(".list").last().click(function(){
       $("#output").show();
       $("#total").text(newOrder.totalPrice());
-
+      $("#orderName").text(newOrder.name);
     });
+
     $("#name").val("");
   });
 });
